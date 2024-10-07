5c09f4b99c270f15f4ac483a0abb4a2ae82392eb
sonicaj <waqarsonic1@gmail.com>
2024-10-07 23:23:38 +0500

    Fix openseachest build (#10)

--- pull.sh
+++ pull.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ex
 VERSION=23.12
-REVISION=2
+REVISION=1
+BACKPORTS='~bpo12+1'
 
 
-wget http://deb.debian.org/debian/pool/main/o/openseachest/openseachest_$VERSION-$REVISION.debian.tar.xz
+wget http://deb.debian.org/debian/pool/main/o/openseachest/openseachest_$VERSION-$REVISION$BACKPORTS.debian.tar.xz
 tar xf openseachest_$VERSION-$REVISION.debian.tar.xz
 rm openseachest_$VERSION-$REVISION.debian.tar.xz
 
